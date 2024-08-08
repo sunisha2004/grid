476f957b9a4d1f090eb15871c41174619208a3fb
sunisha <sunishams2004@gmail.com>
2024-08-08 09:18:49 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,36 @@
+console.log("Javascript variables");
+
+//variable declaration
+var firstName;
+firstName = "Ram"; //variable initialization
+console.log("Name :",firstName);
+
+var a = 10; //variable declaration + initialization
+
+//Data types
+var str = "Hello" //string
+console.log("string :",str);
+console.log("typeof(str): ", typeof(str));
+
+var num = 10; //number
+console.log("number :",num);
+console.log("typpeof(num):",typeof(num));
+
+var bool1 = true; //boolean value
+console.log("bool1 :",bool1);
+console.log("typeof(bool1):", typeof(bool1));
+
+var bool2 = false;
+console.log("bool2 :",bool2);
+
+var nullvalue = null; //null
+console.log("nullvalue :",nullvalue);
+console.log("typeof(null):",typeof(null));
+
+var varundefined = undefined; //undefined
+console.log("varundefined : ",varundefined);
+console.log("typeof(varundefined):",typeof(varundefined));
+
+document.write("HELLOOOOOOO");
+
+console.log("windoow :", window); //global object
