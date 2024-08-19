081a57d16f439859ae33d60ce75b1544a4948055
sunisha <sunishams2004@gmail.com>
2024-08-19 13:10:46 +0530

    19-8-2024 monday

--- script.js
+++ script.js
@@ -108,29 +108,169 @@ console.log("result2:",result2);
 //     counter(0,10);
 // }
 
+// {
+//     //program to show currrent time
+//     function putZero(sec){
+//         return sec<10? "0"+sec:sec;
+//     }
+
+//     function show() {
+//         var time = document.getElementById('time');
+//         var dt = new Date();
+//         var hours = dt.getHours();
+//         // if(hours > 12){
+//         //     ampm = "pm";
+//         // }else{
+//         //     ampm = "am";
+//         // }
+//         var ampm = hours>12?"pm":"am";
+
+//         time.innerHTML = putZero(hours%12)+ ":" + putZero(dt.getMinutes())+ ":" + putZero(dt.getSeconds()) +ampm;
+
+//         setTimeout(function () {
+//             show();
+//         },1000);
+
+//     }
+//     show();
+// }
+
 {
-    //program to show currrent time
-    function putZero(sec){
-        return sec<10? "0"+sec:sec;
+    console.log("\n\n\n\n");
+
+    //spread operator
+
+    let arr =[1,2,3,4,5];
+    console.log("array:",arr);
+
+    let array1 = [...arr,6]; //new elemet can add here(6)
+    console.log("array1 :",array1);
+
+    let obj = {
+        name : "myname",
+        email : "abc@gmail.com",
+        country : "india",
+        
+    };
+
+console.log("object :",obj);
+
+let obj1 = {...obj,mark : 50}; //new element add here(mark:50)
+console.log("object1 :",obj1);
+}
+
+{
+    console.log("\n\n\n\n");
+
+    //Destructuring
+
+    let arr = [1,2,3,4,5];
+    console.log("array:",arr);
+
+    let [a,b,c,d,e] = arr;
+    console.log("a :",a);
+    console.log("b :",b);
+    console.log("c :",c);
+    console.log("d :",d);
+
+    let obj = {
+        name : "myname",
+        email : "abc@gmail.com",
+        country : "india",
+    };
+    console.log("obj : ",obj);
+
+    let {email, country,name} = obj;
+    console.log("name :",name);
+    console.log("email :",email);
+    console.log("country :",country);
+
+    {
+        //nested destructuring
+
+        let matrix = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+        ];
+        console.log("matrix :",matrix);
+
+        let [row1,row2,[g,h,i]] = matrix;
+        console.log("row1",row1);
+        console.log("row2",row2);
+
+        let [a,b,c] = row1;
+        console.log("a",a);
+        console.log("b",b);
+        console.log("c",c);
     }
+}
 
-    function show() {
-        var time = document.getElementById('time');
-        var dt = new Date();
-        var hours = dt.getHours();
-        // if(hours > 12){
-        //     ampm = "pm";
-        // }else{
-        //     ampm = "am";
-        // }
-        var ampm = hours>12?"pm":"am";
+{
+    console.log("\n\n\n\n")
 
-        time.innerHTML = putZero(hours%12)+ ":" + putZero(dt.getMinutes())+ ":" + putZero(dt.getSeconds()) +ampm;
+    //array methods
 
-        setTimeout(function () {
-            show();
-        },1000);
+    let arr = [
+        {
+            name : "john",
+            email : "john@gmail.com",
+            age : 16,
+            mark : 30
+        },
+        {
+            name : "jack",
+            email : "jack@gmail.com",
+            age : 25,
+            mark : 41
+        },
+        {
+            name : "paul",
+            email : "paul@gmail.com",
+            age : 18,
+            mark : 23
+        }
+    ];
 
-    }
-    show();
+    //foreach
+
+    console.log("foreach");
+    let forechresult = arr.forEach((element) =>{
+        console.log("element : ",element);
+        return element.age == 25;
+    });
+    console.log("foreach result :",forechresult);
+
+    console.log("\n\n\n\n");
+
+    //find
+    console.log("find");
+    let findvalue = arr.find ((element) =>{
+        console.log("element :",element);
+        return element.age ==25;
+
+    });
+    console.log("find value :",findvalue);
+
+    console.log("\n\n\n\n\n");
+     //map
+
+     console.log("map");
+     let mappedvalues = arr.map((element) =>{
+        console.log("elements :",element);
+        
+        return element.mark + 10;
+     });
+    
+     console.log("mapped values : ",mappedvalues);
+
+     console.log("\n\n\n\n");
+
+     //reduce
+     console.log("reduce");
+     let sum = arr.reduce((total , element) => {
+        return total + element.mark;
+
+     },0);
+     console.log("sum :",sum);
 }
